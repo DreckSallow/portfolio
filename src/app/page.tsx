24a12baae93a3d4a6191eae5d1956fc04b883b9c
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-04-19 16:13:24 -0500

    feat: add animations to homapage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import InViewCore from "@/components/animations/core/";
 import ContactSection from "@/sections/contact";
 import About from "@/sections/home/about";
 import Projects from "@/sections/home/projects/projects";
@@ -8,26 +9,46 @@ export default function Home() {
 			<section className="flex-center section bg-base text-normal">
 				<div className="flex flex-col max-w-4xl">
 					<div>
-						<h1 className="bg-gradient-to-r from-sky-500 to-emerald-500 bg-clip-text text-transparent text-5xl md:text-7xl font-extrabold">
+						<InViewCore
+							type="h1"
+							delay={0.7}
+							initial={{ x: "-1em" }}
+							whileInView={{ x: 0 }}
+							className="bg-gradient-to-r from-sky-500 to-emerald-500 bg-clip-text text-transparent text-5xl md:text-7xl font-extrabold"
+						>
 							Dikson Aranda
-						</h1>
-						<h3 className="text-3xl md:text-5xl text-accent font-bold mt-4">
+						</InViewCore>
+						<InViewCore
+							type="h3"
+							delay={1}
+							initial={{ x: "1em" }}
+							whileInView={{ x: 0 }}
+							className="text-3xl md:text-5xl text-accent font-bold mt-4"
+						>
 							I bring websites to life with code
-						</h3>
+						</InViewCore>
 					</div>
 					<div className="text-start flex-column sm:block">
-						<p className="text-base md:text-lg mt-6 text-normal">
+						<InViewCore
+							delay={1.3}
+							initial={{ x: "-1em" }}
+							whileInView={{ x: 0 }}
+							className="text-base md:text-lg mt-6 text-normal"
+						>
 							I'm frontend developer, passionate about technology, and focused
 							on building better projects. Always for ways to expand my horizons
 							and challenge myself. I'm sure that I can be an important part of
 							your company to build exceptional things.
-						</p>
-						<button
-							role="button"
+						</InViewCore>
+						<InViewCore
+							type="button"
+							delay={1.6}
+							initial={{ y: "1em" }}
+							whileInView={{ y: 0 }}
 							className="rounded-lg bg-blue-400 px-6 py-3 sm:mt-7 mt-6 text-white tracking-wide"
 						>
 							Contact Me
-						</button>
+						</InViewCore>
 					</div>
 				</div>
 			</section>
